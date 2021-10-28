a441e6979ef1956188af76fb6b5d92313acaaf4e
caypoh23 <caypoh23@gmail.com>
2021-10-28 16:43:57 +0500

    feat: models, controllers and repositories added

--- DSCC.7417.API/Startup.cs
+++ DSCC.7417.API/Startup.cs
@@ -1,16 +1,12 @@
+using DSCC._7417.DAL.Context;
+using DSCC._7417.DAL.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DSCC._7417.API
 {
@@ -32,6 +28,11 @@ namespace DSCC._7417.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DSCC._7417.API", Version = "v1" });
             });
+
+            services.AddDbContext<FurnitureDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("FurnitureDB")));
+
+            services.AddScoped<DbContext, FurnitureDbContext>();
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
